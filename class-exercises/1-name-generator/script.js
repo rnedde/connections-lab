46d8a89cd0c1d9ad31dfec2d8ce055c390740f29
rnedde <rnedde@gmail.com>
2024-09-11 13:08:08 -0400

    added in-class exercise 1

---
+++ class-exercises/1-name-generator/script.js
@@ -0,0 +1,49 @@
+
+
+console.log("File is running!");
+
+//name array
+let names = ["Adam", "Steven", "Reverie", "Anna",
+    "Zelong", "Liangsheng", "Miya", "Yael"];
+
+
+
+
+//make sure whole window is loaded first
+//(Event name, callback function("do this"))
+window.addEventListener("load", () => {
+    //all js code goes here
+    console.log("page has loaded");
+
+    //1. Select the HTML element
+    let pickButton = document.getElementById("pick_button");
+
+    //2. listen for the button to be clicked
+    pickButton.addEventListener("click", () => {
+        console.log("button clicked");
+
+        //3. Pick a random name
+        const numNames = names.length;
+        let randomIndex = Math.floor(Math.random()*numNames);
+        console.log(randomIndex);
+        let pickedName = names[randomIndex];
+        console.log(pickedName);
+
+        //4. show name on the html page
+        let pickedNameElement = document.getElementById("picked_name");
+        pickedNameElement.innerHTML = pickedName;
+
+        //5. add animation
+        pickedNameElement.classList.add("picked_animation");
+
+        //remove class after 3s
+        setTimeout(()=>{
+            pickedNameElement.classList.remove("picked_animation");
+        }, 3000)
+    })
+
+
+});
+
+
+
