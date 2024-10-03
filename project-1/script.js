724e912e7275022ff5ea96fa34abc5e86cb14cdb
rnedde <rnedde@gmail.com>
2024-10-03 14:01:39 -0400

    cleaned up ui, added timer module.

--- project-1/script.js
+++ project-1/script.js
@@ -8,26 +8,53 @@ window.addEventListener("load", function () {
     //array of all bodyparts
     let bodyParts = ['head', 'neck', 'pecs', 'delts', 'biceps', 'triceps', 'forearms', 'hands', 'abs', 'obliques', 'quads', 'adductor', 'shins', 'calves', 'feet'];
 
-
     for (let i = 0; i < bodyParts.length; i++) {
-        hover(bodyParts[i]);
-        isClicked(bodyParts[i], i);
+        interaction(i);
     }
 
 
 
 
-
     //color when mouse hovers, white when removed. 
-    function hover(bodyPart) {
+    function interaction(index) {
         //grabs the color from the css variables (thanks, chatGPT)
-        let hoverColor = getComputedStyle(root).getPropertyValue('--' + bodyPart + '-color').trim();
+        let hoveredBodyPart = bodyParts[index];
+        let hoverColor = getComputedStyle(root).getPropertyValue('--' + hoveredBodyPart + '-color').trim();
 
         //composes the element Id string
-        let bodyPartElementId = 'bendi-' + bodyPart;
+        let bodyPartElementId = 'bendi-' + hoveredBodyPart;
         let bodyPartElement = document.getElementById(bodyPartElementId);
-        let muscleGroupElement = document.getElementById('muscle-group-name');
+        let informationDiv = document.getElementById('information');
+        let commonNameElement = document.getElementById('common-name');
+        let muscleGroupElement = document.getElementById('muscle-group');
+        let stretchNameElement = document.getElementById('stretch-name');
+        let stretchDescriptionElement = document.getElementById('stretch-description');
+        let stretchDurationElement = document.getElementById('stretch-duration');
+        let countdownButtonElement = document.getElementById('countdown-button');
+        
 
+        let bodyPart, name, commonName, muscleGroup, stretch, stretchIndex, stretchName, stretchDescription, stretchDuration;
+
+
+        //get data from JSON
+        fetch('./stretches.json')
+            .then(response => response.json())
+            .then(data => {
+                //sets variables from json data
+                bodyPart = data.bodyParts[index];
+                name = bodyPart.name;
+                commonName = bodyPart.commonName;
+                muscleGroup = bodyPart.muscleGroup;
+
+                //gets a random stretch from the list
+                stretchIndex = Math.floor(Math.random() * bodyPart.stretches.length);
+                stretch = bodyPart.stretches[stretchIndex];
+
+                //variables for the randomly selected stretch
+                stretchName = stretch.stretchName;
+                stretchDescription = stretch.instructions;
+                stretchDuration = stretch.duration;
+            })
 
         if (bodyPartElement) {
             //set all body parts to white first.
@@ -35,67 +62,41 @@ window.addEventListener("load", function () {
 
             //when mouse hovers over element, set to its color
             bodyPartElement.addEventListener('mouseover', function () {
-                console.log(bodyPartElementId + " hovered");
                 bodyPartElement.style.fill = hoverColor;
-                muscleGroupElement.innerHTML = bodyPart;
-                muscleGroupElement.style.backgroundColor = hoverColor;
+                commonNameElement.innerHTML = commonName;
+                commonNameElement.style.backgroundColor = hoverColor;
             });
 
             //when mouse moves off, set to white.
             bodyPartElement.addEventListener('mouseout', function () {
-                console.log(bodyPartElementId + " unhovered");
                 bodyPartElement.style.fill = "white";
-                muscleGroupElement.innerHTML = "";
-                muscleGroupElement.style.backgroundColor = "white";
+                commonNameElement.innerHTML = "";
+                commonNameElement.style.backgroundColor = "white";
             });
-        } else {
-            console.error("Element with ID " + bodyPartElementId + " not found!");
-        }
-    }
 
-
-    function isClicked(bodyPart, index) {
-        //grabs the color from the css variables (thanks, chatGPT)
-        let hoverColor = getComputedStyle(root).getPropertyValue('--' + bodyPart + '-color').trim();
-
-        //composes the element Id string
-        let bodyPartElementId = 'bendi-' + bodyPart;
-
-        let bodyPartElement = document.getElementById(bodyPartElementId);
-
-        //get elements to be filled
-        let muscleGroupNameElement = document.getElementById('muscle-group-name');
-        let stretchNameElement = document.getElementById('stretch-name');
-        let stretchDescription = document.getElementById('stretch-description');
-        let stretchDuration = document.getElementById('stretch-duration');
-
-        //get data from json
-
-        if (bodyPartElement) {
-            //set all body parts to white first.
-            bodyPartElement.style.fill = "white";
-
-            //when mouse clicks on element, show information. 
+            //display stretch information on the page when body part is clicked
             bodyPartElement.addEventListener('click', function () {
                 console.log(bodyPartElementId + " clicked");
-                bodyPartElement.style.fill = hoverColor;
-                muscleGroupNameElement.innerHTML = bodyPart;
-                muscleGroupNameElement.style.backgroundColor = hoverColor;
-                fetch('./stretches.json')
-                .then(response=>response.json())
-                .then(data => {
-                    let bodyPart = data.bodyParts[index];
-                    let name = bodyPart.name;
-                    let commonName = bodyPart.commonName;
-                    let muscleGroup = bodyPart.muscleGroup;
-                    console.log(name,commonName,muscleGroup)
-                })
+                informationDiv.style.visibility='visible';
+                muscleGroupElement.innerHTML = muscleGroup;
+                stretchNameElement.innerHTML = stretchName;
+                stretchDescriptionElement.innerHTML = stretchDescription;
+                stretchDurationElement.innerHTML = stretchDuration;
 
             });
+
+            //countdown madness
+            countdownButtonElement.addEventListener('click', function(){
+                console.log('countdown clicked');
+            })
+
+
+
 
         } else {
             console.error("Element with ID " + bodyPartElementId + " not found!");
         }
+
     }
 
 })
